3396e0881a29ef025e4e3de3bf2cafeda2d6d354
David Gornowicz <davidgornowicz@gmail.com>
2022-08-09 20:29:17 -0400

    progress

--- app.py
+++ app.py
@@ -1,12 +1,43 @@
-# dependency
-from flask import flask
+# dependencies
+import datetime as dt 
+import numpy as np 
+import pandas as pd 
 
-# create new flask instance
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import flask, jsonify
+
+# access SQLite database
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect database into classes
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+# save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+#create session link
+session = Session(engine)
+
+# create flask app
 app = Flask(__name__)
 
-# define root
+# define root ("welcome route")
 @app.route('/')
 
-# create function
-def hello_world():
-    return 'Hello World!'
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
+    Available Routes:
+    /api/v1.0/precipitation
+    /api/v1.0/stations
+    /api/v1.0/tobs
+    /api/v1.0/temp/start/end
+    ''')
+    )
